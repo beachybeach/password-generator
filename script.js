f5de967f82991f79b14fadcd9f856d0e7c3dff67
Riley <rsbeach97@gmail.com>
2021-05-19 21:33:09 -0700

    validates if user selects no pass critera

--- script.js
+++ script.js
@@ -3,6 +3,11 @@
 const letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 const special = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
+let lower;
+let upper;
+let nums;
+let spec;
+
 
 const generatePassword = () => {
   const len = window.prompt('How many characters would you like your password? Choose between 8 and 30');
@@ -11,28 +16,36 @@ const generatePassword = () => {
   } else if (len < 8 || len > 30) {
     window.alert('Password must be between 8 and 30!');
   }
-  let password = [];
-  const lower = window.prompt('Would you like to use lowercase letters?');
-  if (lower == 'Yes' || lower == "yes" || lower == "YES") {
+  //let password = [];
+  else {
+    let lower = window.confirm('Would you like to use lowercase letters?');
+    let upper = window.confirm('Would you like to use uppercase letters?');
+    let nums = window.confirm('Would you like to use numbers?');
+    let spec = window.confirm('Would you like to use special characters??');
+  } if (!lower && !upper && !nums && !spec) {
+    window.alert('You need to enter at least one password element!');
+  }
+  /*if (lower == 'Yes' || lower == "yes" || lower == "YES") {
     password.push(letters[Math.floor(Math.random() * letters.length)]);
     console.log(password);
   }
-  const upper = window.prompt('Would you like to use uppercase letters?');
+
   if (upper == 'Yes' || upper == "yes" || upper == "YES") {
     password.push(letters[Math.floor(Math.random() * letters.length)].toLocaleUpperCase());
     console.log(password);
   }
-  const nums = window.prompt('Would you like to use numbers?');
+
   if (nums == 'Yes' || nums == "yes" || nums == "YES") {
     password.push(numbers[Math.floor(Math.random() * numbers.length)]);
     console.log(password);
   }
-  const spec = window.prompt('Would you like to use special characters??');
+
   if (spec == 'Yes' || spec == "yes" || spec == "YES") {
     password.push(special[Math.floor(Math.random() * letters.length)]);
     console.log(password);
   }
   return password.join('');
+*/
 }
 
 // Get references to the #generate element
