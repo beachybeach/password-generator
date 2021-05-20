7d2d879e7f5ef01967686592908ca326e7cc993a
Riley <rsbeach97@gmail.com>
2021-05-19 21:06:35 -0700

    creates single upper and lower case letter when yes is chosen and adds to array

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 // Assignment code here
 
+const letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const special = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
+
 const generatePassword = () => {
-  const pass = '';
+  let password = [];
   const lower = window.prompt('Would you like to use lowercase letters?');
-  if (lower == 'Yes') {
-    console.log('nice we made it this far lol');
+  if (lower == 'Yes' || lower == "yes" || lower == "YES") {
+    password.push(letters[Math.floor(Math.random() * letters.length)]);
+    console.log(password);
   }
-  window.prompt('Would you like to use uppercase letters?');
+  const upper = window.prompt('Would you like to use uppercase letters?');
+  if (upper == 'Yes' || upper == "yes" || upper == "YES") {
+    password.push(letters[Math.floor(Math.random() * letters.length)].toLocaleUpperCase());
+    console.log(password);
+  }
   window.prompt('Would you like to use numbers?');
   window.prompt('Would you like to use special characters??');
 }
